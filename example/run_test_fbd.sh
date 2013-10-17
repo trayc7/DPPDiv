2a1aa8aa0ec00121da79ee9a7360f94836af3bfb
Tracy Heath <tracyh@berkeley.edu>
2013-10-17 11:59:34 -0700

    updated example script

--- example/run_test_fbd.sh
+++ example/run_test_fbd.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+## This executes a run under a strict clock using the FBD model for fossil calibration
 ./dppdiv-seq-sse -in test_seq.dat -tre test_tre.phy -cal test_fos.cal -tga -clok -n 1000000 -sf 100 -pf 1000 -out test_fbd 
+
+## This runs the same analysis but under the prior by returning a constant value for the likelihood
+./dppdiv-seq-sse -in test_seq.dat -tre test_tre.phy -cal test_fos.cal -tga -clok -n 1000000 -sf 100 -pf 1000 -out test_fbd.pr -rnp
